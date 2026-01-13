321e49a9d6f48f3c2b9f9edbb4c2853a308ce5f9
Darknight123prog <satishkumargupta6658@gmail.com>
2026-01-13 11:25:26 +0530

    fix 02

--- frontend/Pages/Home.jsx
+++ frontend/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div>
       Home
+      <div className='gap-3 border-2 border-black'>
+      <Link to='/payForm'>pay</Link>
+      </div>
     </div>
   )
 }
